2b179b95e650c95426127d920337b06337f62dd5
igore <igor198811@gmail.com>
2018-04-18 10:25:10 +0300

    stream_socket_client

--- project/src/Core/Container/Socket.php
+++ project/src/Core/Container/Socket.php
@@ -20,21 +20,17 @@ class Socket extends AbstractContainerItem
     public function call(SocketRequest $request): SocketResponse
     {
         //todo tothink - every call will create a new socket??
-        //better use most common class
-        //todo tothink - stream_socket_client - is more preferable than fsockopen - it older and by fact is just a copy
-        $connect = fsockopen($request->getHost(), $request->getPort(), $errno, $errstr, $request->getTimeout());
-        //yoda notation is better
+        $connect = stream_socket_client($request->getHost().':'. $request->getPort(), $errno, $errstr);
+        stream_set_timeout($connect,$request->getTimeout());
         if (false === $connect) {
             throw new \RuntimeException($request->getHost().':'.$request->getPort().' - not connect');
         }
-        stream_set_timeout($connect,$request->getTimeout());
         $out = $request->getMethod() . ' ' . $request->getUrl() . ' ' . $request->getHttpVersion() . "\r\n";
         $out .= 'Host: ' . $request->getHost() . "\r\n";
         $out .= "Connection: Close\r\n\r\n";
         $response = new SocketResponse();
         fwrite($connect, $out);
         while (!feof($connect)) {
-            stream_get_meta_data($connect);
             $response->add(fread($connect, 128));
         }
         fclose($connect);
