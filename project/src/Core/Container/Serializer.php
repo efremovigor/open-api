fc8b273bcd9a2ea0ea95ed225391e50f122ff899
igore <igor198811@gmail.com>
2018-04-21 12:31:16 +0300

    вынес в фасад

--- project/src/Core/Container/Serializer.php
+++ project/src/Core/Container/Serializer.php
@@ -6,7 +6,6 @@ use Core\Container\Entity\PropertyAccessInterface;
 
 
 /**
- * todo:много копипаста, нужна оптимизация на уровне Symfony
  * todo tothink is really need so complex and magic class
  * Class Serializer
  * @package Helpers
@@ -39,106 +38,28 @@ class Serializer
                      * object -> object
                      */
                     case \is_object($source) && $source instanceOf PropertyAccessInterface:
-                        foreach($source->getProperties() as $property) {
-                            $setMethod = $this->setMethod($property);
-                            $addMethod = $this->addMethod($property);
-                            $getMethod = $this->getMethod($property);
-                            /**
-                             * Добавляет элементы если свойство в объекте - это массив
-                             */
-                            if(method_exists($source, $getMethod) &&
-                                \is_array($source->$getMethod()) &&
-                                method_exists($subject, $addMethod)
-                            ) {
-                                if($this->isAddable($params) === false && \count($source->$getMethod()) > 0) {
-                                    continue;
-                                }
-                                foreach($source->$getMethod() as $subValue) {
-                                    $subject->$addMethod($subValue);
-                                }
-                                continue;
-                            }
-                            /**
-                             * Рекурсивно вызывается,если св-во subject является обьектом
-                             */
-                            if(method_exists($source, $getMethod) &&
-                                method_exists($subject, $getMethod) &&
-                                \is_object($subject->$getMethod()) &&
-                                (\is_array($source->$getMethod()) || \is_object($source->$getMethod()))
-                            ) {
-                                $this->normalize($source->$getMethod(), $subject->$getMethod());
-                                continue;
-                            }
-                            /**
-                             * Простой сет свойства, если они совпадают по имени
-                             */
-                            if(method_exists($subject, $setMethod)) {
-                                if($this->isNullable($params) === false && $source->$getMethod() === null) {
-                                    continue;
-                                }
-                                if($this->isRewritable($params) === false && $subject->$getMethod() !== null) {
-                                    continue;
-                                }
-                                $subject->$setMethod($source->$getMethod());
-                                continue;
-                            }
-                        }
+                        $this->objectToObject($source, $subject,$params);
                         break;
                     /**
                      * array -> object
                      */
                     case \is_array($source):
-                        foreach($source as $key => $value) {
-                            $setMethod = $this->setMethod($key);
-                            $addMethod = $this->addMethod($key);
-                            $getMethod = $this->getMethod($key);
-                            /**
-                             */
-                            if(\is_array($value) && method_exists($subject, $addMethod)) {
-                                if($this->isAddable($params) === false && \count($value) > 0) {
-                                    continue;
-                                }
-                                foreach($value as $subValue) {
-                                    $subject->$addMethod($subValue);
-                                }
-                                continue;
-                            }
-                            /**
-                             * Рекурсивно вызывается,если св-во subject является обьектом
-                             */
-                            if((\is_array($value) || \is_object($value)) &&
-                                method_exists($subject, $getMethod) &&
-                                \is_object($subject->$getMethod())
-                            ) {
-                                $this->normalize($value, $subject->$getMethod($value));
-                                continue;
-                            }
-                            /**
-                             * Простой сет свойства, если они совпадают по имени
-                             */
-                            if(method_exists($subject, $setMethod)) {
-                                if($this->isNullable($params) === false && $value === null) {
-                                    continue;
-                                }
-                                if($this->isRewritable($params) === false && $subject->$getMethod() !== null) {
-                                    continue;
-                                }
-                                $subject->$setMethod($value);
-                                continue;
-                            }
-                        }
+                        $this->arrayToObject($source,$subject,$params);
                         break;
                     /**
                      * json -> object
                      */
                     case $this->is_json($source):
-                        $this->normalize(json_decode($source, true), $subject);
+                        $this->normalize(json_decode($source, true), $subject,$params);
                         break;
                 }
                 break;
+            /**
+             * Создает класс по имени и рекурсивно вызываем
+             */
             case \is_string($subject):
                 if(class_exists($subject)) {
-                    $subject = $this->normalize($source, new $subject());
+                    $subject = $this->normalize($source, new $subject(),$params);
                 }
                 break;
             case \is_array($subject) || $subject === null:
@@ -147,48 +68,7 @@ class Serializer
                      * object -> array
                      */
                     case \is_object($source) && $source instanceOf PropertyAccessInterface:
-                        if($subject === null) {
-                            $subject = [];
-                        }
-                        foreach($source->getProperties() as $property) {
-                            if(!array_key_exists($property, $subject)) {
-                                $subject[$property] = null;
-                            }
-                            $getMethod = $this->getMethod($property);
-
-                            if(\is_array($source->$getMethod())) {
-
-                                if(!empty($subject[$property])) {
-                                    if($this->isAddable($params) === false && \count($source->$getMethod()) > 0) {
-                                        continue;
-                                    }
-                                    $subject[$property] = array_merge($source->$getMethod(), $subject[$property]);
-                                } else {
-                                    $subject[$property] = $source->$getMethod();
-                                }
-                                continue;
-                            }
-                            /**
-                             * Рекурсивно вызывается,если св-во subject является обьектом
-                             */
-                            if(method_exists($source, $getMethod) &&
-                                (\is_array($source->$getMethod()) || \is_object($source->$getMethod()))
-                            ) {
-                                $subject[$property] = $this->normalize($source->$getMethod(), $subject[$property]);
-                                continue;
-                            }
-                            /**
-                             * Простой сет свойства, если они совпадают по имени
-                             */
-                            if($this->isNullable($params) === false && $source->$getMethod() === null) {
-                                continue;
-                            }
-                            if($this->isRewritable($params) === false && $subject[$property] !== null) {
-                                continue;
-                            }
-                            $subject[$property] = $source->$getMethod();
-                            continue;
-                        }
+                        $this->objectToArray($source,$subject,$params);
                         break;
                     /**
                      * array -> array
@@ -199,7 +79,7 @@ class Serializer
                              * Если элемент массива - массив, и он определен в субьекте - то лезем внутрь
                              */
                             if(\is_array($element) && isset($subject[$key])) {
-                                $subject[$key] = $this->normalize($element, $subject[$key]);
+                                $subject[$key] = $this->normalize($element, $subject[$key],$params);
                             } else {
                                 $subject[$key] = $element;
                             }
@@ -237,6 +117,147 @@ class Serializer
         }
 
         return $source;
+    }
+
+
+    /**
+     * Переливает обьект в обьект
+     * @param PropertyAccessInterface $source
+     * @param object $subject
+     * @param array $params
+     * @return void
+     */
+    private function objectToObject(PropertyAccessInterface $source, object &$subject, array $params = []): void
+    {
+        foreach($source->getProperties() as $property) {
+            $setMethod = $this->setMethod($property);
+            $addMethod = $this->addMethod($property);
+            $getMethod = $this->getMethod($property);
+            /**
+             * Добавляет элементы если свойство в объекте - это массив
+             */
+            if(method_exists($source, $getMethod) &&
+                \is_array($source->$getMethod()) &&
+                method_exists($subject, $addMethod)
+            ) {
+                if($this->isAddable($params) === false && \count($source->$getMethod()) > 0) {
+                    continue;
+                }
+                foreach($source->$getMethod() as $subValue) {
+                    $subject->$addMethod($subValue);
+                }
+                continue;
+            }
+            /**
+             * Рекурсивно вызывается,если св-во subject является обьектом
+             */
+            if(method_exists($source, $getMethod) &&
+                method_exists($subject, $getMethod) &&
+                \is_object($subject->$getMethod()) &&
+                (\is_array($source->$getMethod()) || \is_object($source->$getMethod()))
+            ) {
+                $this->normalize($source->$getMethod(), $subject->$getMethod());
+                continue;
+            }
+            /**
+             * Простой сет свойства, если они совпадают по имени
+             */
+            if(method_exists($subject, $setMethod)) {
+                if($this->isNullable($params) === false && $source->$getMethod() === null) {
+                    continue;
+                }
+                if($this->isRewritable($params) === false && $subject->$getMethod() !== null) {
+                    continue;
+                }
+                $subject->$setMethod($source->$getMethod());
+                continue;
+            }
+        }
+    }
+
+    private function arrayToObject(array $source, object &$subject, array $params = [])
+    {
+        foreach($source as $key => $value) {
+            $setMethod = $this->setMethod($key);
+            $addMethod = $this->addMethod($key);
+            $getMethod = $this->getMethod($key);
+            /**
+             */
+            if(\is_array($value) && method_exists($subject, $addMethod)) {
+                if($this->isAddable($params) === false && \count($value) > 0) {
+                    continue;
+                }
+                foreach($value as $subValue) {
+                    $subject->$addMethod($subValue);
+                }
+                continue;
+            }
+            /**
+             * Рекурсивно вызывается,если св-во subject является обьектом
+             */
+            if((\is_array($value) || \is_object($value)) &&
+                method_exists($subject, $getMethod) &&
+                \is_object($subject->$getMethod())
+            ) {
+                $this->normalize($value, $subject->$getMethod($value));
+                continue;
+            }
+            /**
+             * Простой сет свойства, если они совпадают по имени
+             */
+            if(method_exists($subject, $setMethod)) {
+                if($this->isNullable($params) === false && $value === null) {
+                    continue;
+                }
+                if($this->isRewritable($params) === false && $subject->$getMethod() !== null) {
+                    continue;
+                }
+                $subject->$setMethod($value);
+                continue;
+            }
+        }
+    }
+
+    private function objectToArray(PropertyAccessInterface $source,array &$subject = [], array $params = []){
+        foreach($source->getProperties() as $property) {
+            if(!array_key_exists($property, $subject)) {
+                $subject[$property] = null;
+            }
+            $getMethod = $this->getMethod($property);
+
+            if(\is_array($source->$getMethod())) {
+
+                if(!empty($subject[$property])) {
+                    if($this->isAddable($params) === false && \count($source->$getMethod()) > 0) {
+                        continue;
+                    }
+                    $subject[$property] = array_merge($source->$getMethod(), $subject[$property]);
+                } else {
+                    $subject[$property] = $source->$getMethod();
+                }
+                continue;
+            }
+            /**
+             * Рекурсивно вызывается,если св-во subject является обьектом
+             */
+            if(method_exists($source, $getMethod) &&
+                (\is_array($source->$getMethod()) || \is_object($source->$getMethod()))
+            ) {
+                $subject[$property] = $this->normalize($source->$getMethod(), $subject[$property]);
+                continue;
+            }
+            /**
+             * Простой сет свойства, если они совпадают по имени
+             */
+            if($this->isNullable($params) === false && $source->$getMethod() === null) {
+                continue;
+            }
+            if($this->isRewritable($params) === false && $subject[$property] !== null) {
+                continue;
+            }
+            $subject[$property] = $source->$getMethod();
+            continue;
+        }
     }
 
     /**
