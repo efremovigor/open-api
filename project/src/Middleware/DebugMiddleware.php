1da5f02ac708e9c2d08c575975a1f397a810003c
igore <igor198811@gmail.com>
2018-04-23 12:02:17 +0300

    process

--- project/src/Middleware/DebugMiddleware.php
+++ project/src/Middleware/DebugMiddleware.php
@@ -10,24 +10,14 @@ namespace Middleware;
 
 
 use Core\Middleware\AbstractMiddleware;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
 
 class DebugMiddleware  extends AbstractMiddleware
 {
 
-    /**
-     * Process an incoming server request and return a response, optionally delegating
-     * response creation to a handler.
-     * @param ServerRequestInterface  $request
-     * @param RequestHandlerInterface $handler
-     * @return ResponseInterface
-     */
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+
+    protected function before():void
     {
         echo "DebugMiddleware - init\r\n";
-        return $handler->handle($request);
     }
 
 
